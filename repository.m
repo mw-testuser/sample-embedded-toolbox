f360a81af08e24061ca13158cb91c75b211a8e7a
test-testperson <98484229+test-testperson@users.noreply.github.com>
2023-08-09 21:29:44 -0400

    Add repository function

---
+++ repository.m
@@ -0,0 +1,5 @@
+function metadata = repository
+metadata.owner = "test-testperson";
+metadata.name = "sample-embedded-toolbox";
+metadata.url = "https://github.com/test-testperson/sample-embedded-toolbox";
+end
